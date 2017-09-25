7eae20549bfad5bff51b8d84b2b199a2c82e0f3f
YETC7 <tyler_726@live.com>
2017-09-25 02:08:02 -0700

    Started server.js

--- controllers/savedArticlesController.js
+++ controllers/savedArticlesController.js
@@ -1,30 +1,31 @@
-const db = require("../models");
+// Can't find "../models" ??
+const db = require("../models/Article");
 
 module.exports = {
 	findAll: function(req,res) {
-		db.Article
+		db
 			.find({})
 			.sort({date: -1})
 			.then(dbModel => res.json(dbModel))
 			.catch(err => res.status(422).json(err));;
-	}
+	},
 
 	findById: function(req, res) {
-    	db.Article
+    	db
     		.findById(req.params.id)
     		.then(dbModel => res.json(dbModel))
     		.catch(err => res.status(422).json(err));
 	},
 
 	create: function(req, res) {
-		db.Article
+		db
 			.create(req.body)
 			.then(dbModel => res.json(dbModel))
 			.catch(err => res.status(422).json(err));
 	},
 
 	remove: function(req, res) {
-		db.Article
+		db
 			.findById({ _id: req.params.id })
 			.then(dbModel => dbModel.remove())
 			.then(dbModel => res.json(dbModel))
